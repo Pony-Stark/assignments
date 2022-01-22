6980fd08bdbe20629e34451231c091ea12c2850d
Pony-Stark <shbm94@gmail.com>
2022-01-22 17:00:35 +0530

    Update counter.dart

--- counter.dart
+++ counter.dart
@@ -1,115 +1,71 @@
-import 'package:flutter/cupertino.dart';
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  void onPressed(BuildContext context) {}
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        //onPressed: (){},
-        child: Icon(Icons.add, size: 35),
-        onPressed: () {},
-      ),
       appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text("Todo"),
+        title: Text(widget.title),
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(5),
-        children: [
-          ActivityCard(
-            header: "Pay fees",
-            date: "3rd Jan",
-            list: "Pay bills",
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ActivityCard extends StatelessWidget {
-  const ActivityCard({
-    required this.header,
-    required this.date,
-    required this.list,
-    Key? key,
-  }) : super(key: key);
-
-  final String header, date, list;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: Container(
-        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-        width: double.infinity,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(
-              width: 20,
-              height: 20,
-              child: Checkbox(
-                onChanged: (value) {},
-                value: false,
-              ),
+            const Text(
+              'You have pushed the button this many times:',
             ),
-            Container(
-              width: 10,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  header,
-                  style: TextStyle(
-                    //color, fontsize, fontweight
-                    color: Colors.orange,
-                    //fontWeight: FontWeight.w900,
-                    fontSize: 18,
-                  ),
-                ),
-                SizedBox(height: 5),
-                Text(date),
-                Text(list),
-              ],
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
-      color: Colors.yellow,
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
